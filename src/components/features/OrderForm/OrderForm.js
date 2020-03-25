232de118e8634d0ff9bcfa078382d1c7f6263893
Rafał Pietrzak <rpietrzak@pixelage.pl>
2020-03-25 23:04:30 +0100

    Add send order future

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -2,25 +2,80 @@ import React from 'react';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import OrderOption from '../OrderOption/OrderOption';
 // import styles from './OrderForm.scss';
-import {Col, Row} from 'react-flexbox-grid';
+import { Col, Row } from 'react-flexbox-grid';
 import pricing from '../../../data/pricing.json';
 import PropTypes from 'prop-types';
+import Button from '../../common/Button/Button';
+import { formatPrice } from '../../../utils/formatPrice';
+import { calculateTotal } from '../../../utils/calculateTotal';
+import settings from '../../../data/settings';
+
+const sendOrder = (options, tripCost, tripDays, tripName, tripCountry) => {
+  let sendIfTrue = true;
+  const worrnings = [];
+  if(options.contact === '') {
+    sendIfTrue = false;
+    worrnings.push('Add contact');
+  }
+  if(options.name === '') {
+    sendIfTrue = false;
+    worrnings.push('Add name');
+  }
+  if(options['start-date'] === '') {
+    sendIfTrue = false;
+    worrnings.push('Add start date');
+  }
+  if (sendIfTrue) {
+    const totalCost = formatPrice(calculateTotal(tripCost, options));
+    const payload = {
+      ...options,
+      totalCost,
+      tripDays,
+      tripName,
+      tripCountry,
+    };
+    const url = settings.db.url + '/' + settings.db.endpoint.orders;
+    const fetchOptions = {
+      cache: 'no-cache',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
+    fetch(url, fetchOptions)
+      .then(function (response) {
+        return response.json();
+      }).then(function (parsedResponse) {
+        const orderSummary = [];
+        for(let item in parsedResponse){
+          orderSummary.push(`${item}: ${parsedResponse[item]}`);
+        }
+        alert(`Order was send!!\nSummary:\n${orderSummary.join('\n')}`);
+      });
+  } else {
+    alert(worrnings.join('\n'));
+  }
+};
 
 const OrderForm = (props) => {
-  const {options, setOrderOption} = props;
+  const { options, setOrderOption, tripCost, tripDays, tripName, tripCountry } = props;
   return (
     <Row>
       {pricing.map(option => (
         <Col md={4} key={option.id}>
-          <OrderOption 
-            currentValue={options[option.id]} 
-            {...option} 
+          <OrderOption
+            currentValue={options[option.id]}
+            {...option}
             setOrderOption={setOrderOption}
           />
         </Col>
       ))}
       <Col xs={12}>
         <OrderSummary {...props} />
+        <Button onClick={() => sendOrder(
+          options, tripCost, tripDays, tripName, tripCountry
+        )}>Order now!</Button>
       </Col>
     </Row>
   );
@@ -29,6 +84,10 @@ const OrderForm = (props) => {
 OrderForm.propTypes = {
   options: PropTypes.object,
   setOrderOption: PropTypes.func,
+  tripCost: PropTypes.string,
+  tripDays: PropTypes.number,
+  tripName: PropTypes.string,
+  tripCountry: PropTypes.string,
 };
 
 export default OrderForm;
