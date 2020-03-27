e54b9cf732b4fb7a84f03f719791feb064e93807
Rafał Pietrzak <rpietrzak@pixelage.pl>
2020-03-27 13:55:26 +0100

    Add HappyHourAd component and test

---
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './HappyHourAd.scss';
+
+class HappyHourAd extends React.Component {
+  static propTypes = {
+    title: PropTypes.string,
+    promoDescription: PropTypes.string,
+  };
+  constructor() {
+    super();
+    setInterval(() => {
+      this.forceUpdate();
+    }, 1000);
+    /* run this.forceUpdate() every second */
+  }
+  getCountdownTime() {
+    const currentTime = new Date();
+    const nextNoon = new Date(
+      Date.UTC(
+        currentTime.getUTCFullYear(),
+        currentTime.getUTCMonth(),
+        currentTime.getUTCDate(), 12, 0, 0, 0
+      ));
+    if (currentTime.getUTCHours() >= 12) {
+      nextNoon.setUTCDate(currentTime.getUTCDate() + 1);
+    }
+    return Math.round((nextNoon.getTime() - currentTime.getTime()) / 1000);
+  }
+  render() {
+    const { title, promoDescription} = this.props;
+    const countdownTime = this.getCountdownTime();
+    return (
+      <div className={styles.component}>
+        <h3 className={styles.title}>
+          {title}
+        </h3>
+        <div className={styles.promoDescription}>
+          {countdownTime <= 60*60*23 ? countdownTime : promoDescription}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HappyHourAd;
--- src/components/layout/Hero/Hero.test.js
+++ src/components/layout/Hero/Hero.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 import Hero from './Hero';
 
 describe('Component Hero', () => {
   it('should render without crashing', () => {
     const component = shallow(
-      <Hero titleText='Lorem ipsum' imageSrc='image.jpg'/>
+      <Hero titleText='Lorem ipsum' imageSrc='image.jpg' />
     );
     expect(component).toBeTruthy();
     console.log(component.debug());
@@ -17,7 +17,7 @@ describe('Component Hero', () => {
     const expectedTitle = 'Lorem ipsum';
     const expectedImage = 'image.jpg';
     const component = shallow(
-      <Hero titleText={expectedTitle} imageSrc={expectedImage}/>
+      <Hero titleText={expectedTitle} imageSrc={expectedImage} />
     );
     const renderedTitle = component.find('.title').text();
     expect(renderedTitle).toEqual(expectedTitle);
@@ -29,5 +29,11 @@ describe('Component Hero', () => {
     expect(component.hasClass('component')).toBe(true);
     expect(component.hasClass('small')).toBe(true);
     expect(component.hasClass('dummy')).toBe(true);
+  });
+  it('should render HappyHourAd', () => {
+    const expectedTitle = 'Lorem ipsum';
+    const expectedImage = 'image.jpg';
+    const component = shallow(<Hero titleText={expectedTitle} imageSrc={expectedImage} />);
+    expect(component.find('HappyHourAd').length).toEqual(1);
   });
 });
